30974456339109b77bc6485c7e904ac9bb828f5e
Daniel Rodríguez <sadasant@users.noreply.github.com>
2022-01-30 21:49:42 +0000

    some improvements

--- generators/2x2s/generate.go
+++ generators/2x2s/generate.go
@@ -2,20 +2,18 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
+	"time"
 )
 
-type Pairs [][2]int
-
-func readPairs(path string) [][2]int {
+func readPairs(path string) [][2]uint16 {
 	content, err := ioutil.ReadFile(path)
 	if err != nil {
 		log.Fatal("Error when opening file: ", err)
 	}
-	var pairs [][2]int
+	var pairs [][2]uint16
 	err = json.Unmarshal(content, &pairs)
 	if err != nil {
 		log.Fatal("Error during Unmarshal(): ", err)
@@ -23,64 +21,45 @@ func readPairs(path string) [][2]int {
 	return pairs
 }
 
-var hPairs [][2]int = readPairs("../horizontal_pairs/horizontal_pairs.json")
-var hMap = [31119 * 31119]bool{}
+const unsigSize uint16 = 31119
+const unsigSize32 uint32 = 31119
 
-var vPairs [][2]int = readPairs("../vertical_pairs/vertical_pairs.json")
-var vMap = [31119 * 31119]bool{}
-
-func toIndex(a, b int) int {
-	return a*31119 + b
-}
+var hPairs [][2]uint16 = readPairs("../horizontal_pairs/horizontal_pairs.json")
+var vPairs [][2]uint16 = readPairs("../vertical_pairs/vertical_pairs.json")
+var vMap = [unsigSize][unsigSize]bool{}
 
 func init() {
 	for _, v := range vPairs {
-		vMap[toIndex(v[0], v[1])] = true
+		vMap[v[0]][v[1]] = true
 	}
-	for _, h := range hPairs {
-		hMap[toIndex(h[0], h[1])] = true
-	}
-	// Garbage collecting
-	// hPairs = [][2]int{}
-	// vPairs = [][2]int{}
 }
 
-type Square [4]int
-
-func is2x2(square Square) bool {
-	a := square[0]
-	b := square[1]
-	c := square[2]
-	d := square[3]
-	// Unique. We're assuming a,b and c,d are good horizontal pairs.
-	if (a == c || a == d) ||
-		(b == c || b == d) {
-		return false
-	}
+func is2x2(a, b, c, d uint16) bool {
 	// Skipping horizontals since we start with them
 	// return isH(Pair{a, b}) && isH(Pair{c, d}) && isV(Pair{a, c}) && isV(Pair{b, d})
-	return vMap[toIndex(a, c)] && vMap[toIndex(b, d)]
+	if a == c || a == d || !vMap[a][c] {
+		return false
+	}
+	// Unique. We're assuming a,b and c,d are good horizontal pairs.
+	if b == c || b == d || !vMap[b][d] {
+		return false
+	}
+	return true
 }
 
 func main() {
 	// Quick test. Both should be true.
-	println(hMap[toIndex(10, 231)])
-	println(vMap[toIndex(22888, 28060)])
-	println(is2x2(Square{10796, 10798, 10818, 10820}))
+	println(vMap[22888][28060])
+	println(is2x2(10796, 10798, 10818, 10820))
 
 	fileName := "2x2s.json"
 	len_hPairs := len(hPairs)
-	base := 4
-	page := 1
-	start := (len_hPairs / base) * (page - 1)
-	end := (len_hPairs / base) * page
-	println(start, end, page, base)
+	end := len_hPairs
+	println("End:", end)
 
-	if start == 0 {
-		err := ioutil.WriteFile(fileName, []byte{}, 0644)
-		if err != nil {
-			log.Fatal("Failed to write", fileName)
-		}
+	err := ioutil.WriteFile(fileName, []byte{}, 0644)
+	if err != nil {
+		log.Fatal("Failed to write", fileName)
 	}
 
 	file, err := os.OpenFile(fileName, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
@@ -93,42 +72,43 @@ func main() {
 		panic(err)
 	}
 
-	mar5manTop := 47379186
-	jTemplate := "[%v,%v,%v,%v],"
-
+	mar5manTop := 47379186 // 47 379 186
+	startTime := time.Now()
 	count := 0
+	println("Running!")
 L:
-	for i := start; i < end; i++ {
-		jsonString := ""
-		iTemplate := ",%s"
-		if i == 0 {
-			iTemplate = "%s"
-		}
-		for j := start; j < end; j++ {
+	for i := 0; i < end; i++ {
+		matches := [][4]uint16{}
+		for j := 0; j < end; j++ {
 			if i == j {
-				continue L
+				continue
 			}
 			h1 := hPairs[i]
 			h2 := hPairs[j]
-			if is2x2(Square{h1[0], h1[1], h2[0], h2[1]}) {
-				jsonString += fmt.Sprintf(jTemplate, h1[0], h1[1], h2[0], h2[1])
-				if count%10000 == 0 {
-					print(i, " ", j, " ", start, "-", end, " ", count, "\r")
-				}
+			if is2x2(h1[0], h1[1], h2[0], h2[1]) {
+				matches = append(matches, [4]uint16{h1[0], h1[1], h2[0], h2[1]})
 				count += 1
 			}
 		}
-		if _, err = file.WriteString(fmt.Sprintf(iTemplate, jsonString[0:len(jsonString)-1])); err != nil {
-			panic(err)
+		if len(matches) == 0 {
+			continue L
 		}
+		if count%10000 == 0 {
+			seconds := int(time.Since(startTime).Seconds())
+			print("\r", i, " ", seconds, " ", count, " minutes left: ", (mar5manTop*seconds)/count/60, "        ")
+		}
+		jsonString, _ := json.Marshal(matches)
+		prefix := ","
+		if i == 0 {
+			prefix = ""
+		}
+		file.Write(append([]byte(prefix), jsonString[1:len(jsonString)-1]...))
 	}
 
+	println("Done!")
 	println(count, "should be", mar5manTop)
 
 	endString := "]"
-	if page != base {
-		endString = ","
-	}
 	if _, err = file.WriteString(endString); err != nil {
 		panic(err)
 	}
