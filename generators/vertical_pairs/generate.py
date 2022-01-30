f36c655b97b69f8da2dd1eafe70b3cd986ecfe7e
Daniel Rodríguez <sadasant@users.noreply.github.com>
2022-01-30 02:35:22 +0000

    vertical_pairs

---
+++ generators/vertical_pairs/generate.py
@@ -0,0 +1,97 @@
+from src import group_props_by, group_by_color, unsig_range, write, time
+from itertools import product
+import functools
+import json
+
+props = json.load(open('../props/props.json'))
+
+
+@functools.lru_cache(maxsize=31119)
+def vertical_match_group_a(n):
+    n_props = props[n]
+    rotations = group_props_by(n_props, "rotation")
+    group_a = (rotations.get("0") or []) + (rotations.get("180") or [])
+    return json.dumps(group_a)
+
+
+@functools.lru_cache(maxsize=31119 * 2)
+def vertical_match_group_b(n_with_inverted):
+    n = n_with_inverted[0]
+    inverted = n_with_inverted[1]
+    n_props = props[n]
+    colors = group_by_color(n_props)
+    # Extracting all the colors with only one property (Group B).
+    one_property_colors = list(map(lambda x: x[0], list(
+        filter(lambda x: len(x) == 1, colors.values()))))
+    # Not including normals (Group B).
+    distributions = group_props_by(one_property_colors, "distribution")
+    group_b_no_normals = distributions.get("CDF") or []
+    # Extracting all the properties have 90 and 270 rotations (Group B).
+    rotations = group_props_by(group_b_no_normals, "rotation")
+    group_b = []
+    if inverted != True:
+        # Removing CDFs from the unsig on the top if they have a rotation of 90 (Group B).
+        group_b = rotations.get("270") or []
+    else:
+        # Removing CDFs from the unsig on the bottom if they have a rotation of 270 (Group B).
+        group_b = rotations.get("90") or []
+    # As long as Group B mirrors one side with the other.
+    copied_group_b = list(map(lambda x: x.copy(), group_b))
+    list(map(lambda x: x.pop("rotation"), copied_group_b))
+    # One side will mirror another side if the multiplier is 1 or above
+
+    def remove_05s(x):
+        if x["multiplier"] != "0.5":
+            x.pop("multiplier")
+    list(map(remove_05s, copied_group_b))
+    return json.dumps(copied_group_b)
+
+
+@functools.lru_cache(maxsize=31119 * 2)
+def is_vertical_pair(pair):
+    top_n = pair[0]
+    bottom_n = pair[1]
+    # Group A.
+    top_group_a = vertical_match_group_a(top_n)
+    bottom_group_a = vertical_match_group_a(bottom_n)
+    # Group B.
+    top_group_b = vertical_match_group_b((top_n, False))
+    bottom_group_b = vertical_match_group_b((bottom_n, True))
+    # If groups A and B are equal between one unsig and the other one.
+    # print("top_group_a", top_group_a)
+    # print("bottom_group_a", bottom_group_a)
+    # print("top_group_b", top_group_b)
+    # print("bottom_group_b", bottom_group_b)
+    return top_group_a == bottom_group_a and top_group_b == bottom_group_b
+
+
+print(is_vertical_pair((26642, 26701)))
+print(is_vertical_pair((12142, 12397)))
+print(is_vertical_pair((16588, 23666)))
+print(is_vertical_pair((1507, 148)))
+print(is_vertical_pair((144, 6)))
+print(is_vertical_pair((23258, 10370)))
+print(is_vertical_pair((22888, 28060)))
+
+# /show numbers:6121,19880,1189,9110 columns:2
+# Should be false!
+# test2x2 = [(6121, 1189), (19880, 9110)]
+# print(is_2x2(test2x2))
+
+
+def generate():
+    all_vertical_pairs = []
+    for indices in product(unsig_range, repeat=2):
+        if indices[0] is not indices[1]:
+            if is_vertical_pair(indices):
+                all_vertical_pairs.append(indices)
+        else:
+            if indices[0] % 1000 is 0:
+                print(indices[0])
+    return all_vertical_pairs
+
+
+time(lambda: write(generate(), "vertical_pairs.json"))
+
+print(is_vertical_pair((6121, 19880)))  # Should be false!
+print(is_vertical_pair((1189, 9110)))  # Should be false!
